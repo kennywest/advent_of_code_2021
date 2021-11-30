b9a049a0eacdf0ff55d03e74ee6a8ebf3c12d75a
Kenneth Westelinck <kenneth.westelinck@aca-it.be>
2021-11-30 08:22:42 +0100

    init

---
+++ src/main/java/advent/of/code/day01/HelloWorld.java
@@ -0,0 +1,12 @@
+package advent.of.code.day01;
+
+public final class HelloWorld {
+
+    private HelloWorld() {
+
+    }
+
+    public static void main(String... args) {
+        System.out.println("hellow world");
+    }
+}
---
+++ src/test/java/advent/of/code/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package advent.of.code.day01;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class HelloWorld {
+
+    @Test
+    public void shouldTest() {
+        System.out.println("hellow world");
+
+        Assertions.assertThat(true).isTrue();
+    }
+}
